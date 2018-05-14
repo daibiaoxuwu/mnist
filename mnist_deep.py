c60f1c9611f7bd3e686b8820b86633120a6bc442
daibiaoxuwu <dujl16@mails.tsinghua.edu.cn>
2018-05-15 00:47:10 +0800

    修改：     mnist_deep.py

--- mnist_deep.py
+++ mnist_deep.py
@@ -25,6 +25,7 @@ https://www.tensorflow.org/get_started/mnist/pros
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from mnistreader import reader
 
 import argparse
 import sys
@@ -126,8 +127,18 @@ def bias_variable(shape):
 
 
 def main(_):
-  # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir)
+  data_sets=reader(patchlength=0,\
+            maxlength=300,\
+            embedding_size=100,\
+            num_verbs=10,\
+            allinclude=False,\
+            shorten=False,\
+            shorten_front=False,\
+            testflag=False,\
+            passnum=0,\
+            dpflag=False)
+
+  
 
   # Create the model
   x = tf.placeholder(tf.float32, [None, 784])
@@ -161,13 +172,12 @@ def main(_):
     for i in range(20000):
       batch = mnist.train.next_batch(50)
       if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print('step %d, training accuracy %g' % (i, train_accuracy))
+        do_evalfake(sess,
+                eval_correct,data_sets,FLAGS.batch_size,
+                images_placeholder,
+                labels_placeholder)
       train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-    print('test accuracy %g' % accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
